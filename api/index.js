b528ab2e85de8cfc9e3cc019268880371d948338
alexandrminskiy <alexandrminskiy@gmail.com>
2023-02-28 14:18:26 +0300

    1.46

--- api/index.js
+++ api/index.js
@@ -1,3 +1,24 @@
-console.log('giuli')
+console.log('work')
 
-// 123
+const $wr = document.querySelector('[data-wr]')
+
+// функция getCatHTML принимает объект кота
+const getCatHTML = (cat) => `
+    <div class="card mb-4 mx-2" style="width: 18rem;">
+                <img src="${cat.image}" class="card-img-top" alt="${cat.name}">
+                <div class="card-body">
+                    <h5 class="card-title">${cat.name}</h5>
+                    ${cat.description}
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            `
+
+fetch('https://cats.petiteweb.dev/api/single/alexandrminskiy/show/')
+  .then((res) => res.json())
+  .then((data) => {
+    //   Обращаемся к контейнеру
+    $wr.insertAdjacentHTML('afterbegin', data.map((cat) => getCatHTML(cat)).join(''))
+
+    console.log({ data })
+  })
